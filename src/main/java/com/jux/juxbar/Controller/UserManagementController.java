5f796476c32508d5d0798c25606feeb4bbc92c4b
jux42 <roujux@gmail.com>
2024-06-26 23:57:22 +0200

    création users + méthodes admin

--- src/main/java/com/jux/juxbar/Controller/UserManagementController.java
+++ src/main/java/com/jux/juxbar/Controller/UserManagementController.java
@@ -7,10 +7,7 @@ import com.jux.juxbar.Service.JuxBarUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
 

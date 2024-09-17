22c8ac23f799dc8e53441e32f9499cdbb75e7e07
jux42 <roujux@gmail.com>
2024-09-17 21:45:17 +0200

    jenkins build test

--- src/main/java/com/jux/juxbar/JuxBarApplication.java
+++ src/main/java/com/jux/juxbar/JuxBarApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 
-//TEST JENKINS AUTOBUILD 7
+//TEST JENKINS AUTOBUILD 8
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @ConfigurationPropertiesScan
 

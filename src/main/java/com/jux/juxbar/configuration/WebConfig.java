867b519acac9dcb77c7fcdc28be46e70958622ee
jux42 <roujux@gmail.com>
2024-09-21 17:37:57 +0200

    container config

--- src/main/java/com/jux/juxbar/configuration/WebConfig.java
+++ src/main/java/com/jux/juxbar/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Autorise les requêtes CORS pour toutes les routes, acceptable en dev, pas en prod
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://192.168.1.200:4200", "https://juxbar-frontend.eu.ngrok.io", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

9d45bd420934233b15c6307d3d19ec6c8128a2bc
jux42 <roujux@gmail.com>
2024-09-21 17:34:32 +0200

    container config

--- src/main/java/com/jux/juxbar/configuration/WebConfig.java
+++ src/main/java/com/jux/juxbar/configuration/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true).maxAge(3600);
+                .allowCredentials(true);
     }
 }

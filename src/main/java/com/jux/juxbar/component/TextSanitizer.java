cf8bbe29b30ea7e13c8e91cbae0b428bb24b8731
jux42 <roujux@gmail.com>
2024-09-22 03:21:18 +0200

    text cleaner

--- src/main/java/com/jux/juxbar/component/TextSanitizer.java
+++ src/main/java/com/jux/juxbar/component/TextSanitizer.java
@@ -13,13 +13,35 @@ public class TextSanitizer {
     }
 
     public static PersonalCocktail sanitizeCocktailText(PersonalCocktail personalCocktail) {
-        personalCocktail.setStrIngredient1(Jsoup.clean(personalCocktail.getStrIngredient1(),Safelist.simpleText()));
-        personalCocktail.setStrIngredient2(Jsoup.clean(personalCocktail.getStrIngredient2(),Safelist.simpleText()));
-        personalCocktail.setStrIngredient3(Jsoup.clean(personalCocktail.getStrIngredient3(),Safelist.simpleText()));
-        personalCocktail.setStrIngredient4(Jsoup.clean(personalCocktail.getStrIngredient4(),Safelist.simpleText()));
-        personalCocktail.setStrIngredient5(Jsoup.clean(personalCocktail.getStrIngredient5(),Safelist.simpleText()));
-        personalCocktail.setStrIngredient6(Jsoup.clean(personalCocktail.getStrIngredient6(),Safelist.simpleText()));
-        personalCocktail.setStrIngredient7(Jsoup.clean(personalCocktail.getStrIngredient7(),Safelist.simpleText()));
+
+
+        personalCocktail.setStrIngredient1( personalCocktail.getStrIngredient1().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient1(),Safelist.simpleText()));
+
+        personalCocktail.setStrIngredient1( personalCocktail.getStrIngredient1().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient2(),Safelist.simpleText()));
+
+        personalCocktail.setStrIngredient2( personalCocktail.getStrIngredient2().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient3(),Safelist.simpleText()));
+
+        personalCocktail.setStrIngredient3( personalCocktail.getStrIngredient3().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient1(),Safelist.simpleText()));
+
+        personalCocktail.setStrIngredient4( personalCocktail.getStrIngredient4().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient4(),Safelist.simpleText()));
+
+        personalCocktail.setStrIngredient5( personalCocktail.getStrIngredient5().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient5(),Safelist.simpleText()));
+
+        personalCocktail.setStrIngredient6( personalCocktail.getStrIngredient6().isEmpty() ?
+                "":
+                Jsoup.clean(personalCocktail.getStrIngredient6(),Safelist.simpleText()));
 
         return personalCocktail;
     }

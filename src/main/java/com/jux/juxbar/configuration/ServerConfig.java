4559058199dc4b5c8726464766c31e5e88bf1ef6
jux42 <roujux@gmail.com>
2024-10-17 03:00:25 +0200

    FIX : account deletion bad sql request

--- src/main/java/com/jux/juxbar/configuration/ServerConfig.java
+++ src/main/java/com/jux/juxbar/configuration/ServerConfig.java
@@ -44,3 +44,4 @@ public class ServerConfig {
         return connector;
     }
 }
+
--- src/main/java/com/jux/juxbar/configuration/WebConfig.java
+++ src/main/java/com/jux/juxbar/configuration/WebConfig.java
@@ -11,9 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://192.168.1.200:4200", "https://juxbar-frontend.eu.ngrok.io", "https://localhost:4200")
+                .allowedOrigins(
+                        "https://192.168.1.200:4200",
+                        "https://juxbar-frontend.eu.ngrok.io",
+                        "https://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
+

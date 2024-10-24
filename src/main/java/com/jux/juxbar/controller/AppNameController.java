eb800d1a1d521c2c6e8bbbc17a64be2bb181a91b
jux42 <roujux@gmail.com>
2024-10-25 01:18:10 +0200

    autosigned ssl

--- src/main/java/com/jux/juxbar/controller/AppNameController.java
+++ src/main/java/com/jux/juxbar/controller/AppNameController.java
@@ -2,8 +2,12 @@ package com.jux.juxbar.controller;
 
 import com.jux.juxbar.configuration.AppName;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.parser.Host;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Slf4j
 @RestController
@@ -26,5 +30,9 @@ public class AppNameController {
         return this.appName.getName();
     }
 
+    @GetMapping("/localip")
+    String getLocalIp() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostAddress();
+    }
 
 }
